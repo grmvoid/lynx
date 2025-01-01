7e2ce553f57c9a2ee1058f429899e2c62841466d
Dominik Szamburski <dominikszamburski99@gmail.com>
2025-01-01 09:24:18 +0100

    Update copyright date

--- src/hashdom/index.js
+++ src/hashdom/index.js
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2022 Dominik Szamburski
+ * Copyright (c) 2022-2025 Dominik Szamburski
  *
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
